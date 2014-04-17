e2d7857b5f466ad325a99d9f468344fb700f4bdf
Rémi Tessier <rt012@hdm-stuttgart.de>
2014-04-17 11:22:15 +0200

    Exercise Overview angepasst

--- WorkoutLog/src/com/workout/log/ExerciseOverview.java
+++ WorkoutLog/src/com/workout/log/ExerciseOverview.java
@@ -42,15 +42,15 @@ public class ExerciseOverview extends ActionBarActivity implements OnItemLongCli
     private CharSequence mTitle;
     CustomDrawerAdapter adapter;
 
-    List<DrawerItem> dataList;
-	
+    
+    MenueListe l = new MenueListe();
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.exercise_overview);
 		
 		// Initializing
-        dataList = new ArrayList<DrawerItem>();
+       
         mTitle = mDrawerTitle = getTitle();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
@@ -59,21 +59,9 @@ public class ExerciseOverview extends ActionBarActivity implements OnItemLongCli
                     GravityCompat.START);
         
      // Add Drawer Item to dataList
-        dataList.add(new DrawerItem("Message", R.drawable.ic_action_email));
-        dataList.add(new DrawerItem("Likes", R.drawable.ic_action_good));
-        dataList.add(new DrawerItem("Games", R.drawable.ic_action_gamepad));
-        dataList.add(new DrawerItem("Lables", R.drawable.ic_action_labels));
-        dataList.add(new DrawerItem("Search", R.drawable.ic_action_search));
-        dataList.add(new DrawerItem("Cloud", R.drawable.ic_action_cloud));
-        dataList.add(new DrawerItem("Camara", R.drawable.ic_action_camera));
-        dataList.add(new DrawerItem("Video", R.drawable.ic_action_video));
-        dataList.add(new DrawerItem("Groups", R.drawable.ic_action_group));
-        dataList.add(new DrawerItem("Import & Export", R.drawable.ic_action_import_export));
-        dataList.add(new DrawerItem("About", R.drawable.ic_action_about));
-        dataList.add(new DrawerItem("Settings", R.drawable.ic_action_settings));
-        dataList.add(new DrawerItem("Help", R.drawable.ic_action_help));
+       
 		
-        adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item, dataList);
+        adapter = new CustomDrawerAdapter(this, R.layout.custom_drawer_item, l.getDataList());
 
         mDrawerList.setAdapter(adapter);
         
